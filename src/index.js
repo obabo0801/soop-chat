5f5ec5d9e22e708ca05acfcb3b7d7bf41311401b
obabo0801 <obabo0801@gmail.com>
2026-05-24 21:36:11 +0900

    UPDATE 1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,44 @@
-console.log('start');
+// 스트리머 아이디
+const BJ_ID = 'niniming';
+
+// 필요하면 브라우저 쿠키 넣기
+const COOKIE = ''
+
+// 비밀번호 방송이면 입력
+const PASSWORD = '';
+
+import * as config from '#soop/config';
+
+
+async function getLiveInfo(bjId) {
+    const url = `${config.DOMAIN.live}/afreeca/player_live_api.php?bjid=${bjId}`;
+
+    const body = new URLSearchParams({
+        bid: bjId,
+//        bno: '0',
+//        pwd: PASSWORD,
+        ...config.BODY
+    });
+
+    const headers = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+    }
+
+    const res = await fetch(url, {
+        method: 'POST',
+        headers,
+        body
+    });
+
+    return res.json();
+}
+
+(async () => {
+    const data = await getLiveInfo(BJ_ID);
+
+    console.log(data.RESULT);
+    console.log(data.CHANNEL?.BNO);
+    console.log(data.CHANNEL?.CHATNO);
+    console.log(data.CHANNEL?.CHDOMAIN);
+    console.log(data.CHANNEL?.FTK);
+})();
---
+++ src/soop/config.js
@@ -0,0 +1,25 @@
+export const DOMAIN = {
+    live: 'https://live.sooplive.com',
+};
+
+export const USER_AGENT = {
+
+}
+
+export const BODY = {
+    type: 'live',
+    player_type: 'html5',
+    stream_type: 'common',
+    quality: 'HD',
+    mode: 'landing',
+    from_api: '0',
+    is_revive: false
+};
+
+export const DELIMITER = {
+    ESC: '\x1b\t',
+    FF: '\x0c',
+    DC1: '\x11',
+    DC2: '\x12',
+    ACK: '\x06'
+};
